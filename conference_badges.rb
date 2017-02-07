fce04301975f3a3b943d0e22072ec021792758fa
Tim Freeman <d.timothy.freeman@gmail.com>
2017-02-07 13:00:05 -0500

    Badges and Schedules -- Passed Tests

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+	"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+	badge_batch_list = []
+	array.each {|name| badge_batch_list.push(badge_maker(name))}
+	badge_batch_list
+end
+
+def assign_rooms(array)
+	room_assignments = []
+	array.each_with_index {|name, index| room_assignments.push("Hello, #{name}! You'll be assigned to room #{index+1}!")}
+	room_assignments
+end
+
+def printer(array)
+	badges = batch_badge_creator(array)
+	badges.each {|badge| puts badge}
+	room_assignments_list = assign_rooms(array)
+	room_assignments_list.each {|assignment| puts assignment}
+end
